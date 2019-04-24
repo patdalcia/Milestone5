c02498a1eac3739415b86dc852703c8520aedf02
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-23 19:35:52 -0700

    Fixing postHandler

--- postHandler.php
+++ postHandler.php
@@ -5,7 +5,7 @@
     $title=trim($_POST['title']);
     $body=trim($_POST['body']);
     $category=trim($_POST['category']);
-    
+    $results = 0;
     if (!$title || !$body || !$category)
     { 
         echo '<script language="javascript">alert("Input fields were left blank! Click OK to try again.")</script>';
@@ -14,13 +14,37 @@
     $db = dbConnect();
     session_start();
     echo 'Made connections and session';
-    $query = "INSERT INTO posts (title, content, date_created, user_id, catID) VALUES (?, ?, NOW(), ?, ?)";
+    
+    $query = "SELECT catID FROM categories WHERE name = ?";
     $stmt = $db->prepare($query);
-    $stmt->bind_param('ssis', $title, $body, $_SESSION['ID'], $category);
-    if($stmt->execute())
-    {
-        echo 'Post has been saved succsesfully!';
-    } else { echo 'Error: Could not save post';}
-
-
+    $stmt->bind_param('s', $category);
+  
+    if($stmt->execute()){
+        $stmt->store_result();
+        if($stmt->num_rows > 0){
+            $stmt->bind_result($results);
+            if($stmt->fetch()){
+                $stmt->free_result();
+                
+                $query = "INSERT INTO posts (title, content, user_id, catID, date_created) VALUES (?, ?, ?, ?, NOW())";
+                $stmt = $db->prepare($query);
+                $stmt->bind_param('ssis', $title, $body, $_SESSION['ID'], $results);
+                if($stmt->execute())
+                {
+                    echo 'Post has been saved succsesfully!';
+                } else { echo 'Error: Could not save post';}
+            }
+        } else{  //Category could not be found
+            
+            $query = "INSERT INTO categories (name, date) VALUES (?, NOW())";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('s', $category);
+            if($stmt->execute())
+            {
+                echo 'No Category found with that name...Created new category!';
+            } else { echo 'Error: Could not create new category';}
+        }
+    }
+    
+   
 ?>
