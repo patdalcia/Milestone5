68e399f165456a670542353d46f57d43addc4de6
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 14:16:03 -0700

    ee

--- editPost.php
+++ editPost.php
@@ -1,4 +1,4 @@
-
+<?php createSelect();?>
 
 <?php
 function getPost($postId){
@@ -116,46 +116,3 @@ if(isset($_POST["editPostCreate"])){
 ?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<link rel="stylesheet" href="styles/styles.css">
-<title>My Blog Page</title>
-
-</head>
-<body>
-<h2>My Blog Page</h2>
-
-<form action="" method="post" id="usrform">
-<table style="border: 0px;"> 
-
- <tr> 
- <td>Title of Post: <input type="text" name="title" value="<?php if(isset($title)){echo $title;}?>" maxlength="30"></td>
- </tr>
- <tr>
- <td>Body of post: <textarea rows="4" cols="50" name="body" form="usrform">Enter text here...</textarea></td>
- </tr>
-  <tr> 
- 	<td><?php createSelect();?></td>
-  </tr>
-  <tr>
-  <td><input type="submit" value="Create Post"></td>
-  </tr>
- </table>
-</form>
-
-<form action="" method="post" id="showCategories">
-<table style="border: 0px;">
-<tr>
-<td> 		</td>
-<td><input type="submit" name="categories"value="View Category List"></td>
-</tr>
-
-</table>
-</form>
-<br>
-
-
-</body>
-</html>
