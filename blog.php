2f23d5e86081dd861a5614c73735d982410c28c9
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-24 23:04:20 -0700

    aa

--- blog.php
+++ blog.php
@@ -111,7 +111,7 @@ if(isset($_POST['create'])) {
                 </select>
                 </form>';
        
-
+        $db->close();
     
 } else if (isset($_POST['Vcategories'])){
     require 'functions/myfuncs.php';
@@ -187,46 +187,41 @@ if(isset($_POST["catSelect"]) && $_POST["catSelect"] != 0){
 
 */
 if(isset($_POST["ChooseCat"])){
-    echo 'Inside isset';
-    $cat = (int)$_POST["cat"];
-    $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param("i", $cat);
-    if($stmt->execute()){
-        $stmt->bind_result($result);
-        if($stmt->fetch()){
-            echo '<table border="0" cellspacing="2" cellpadding="2">
+    require 'functions/myfuncs.php';
+    $db = dbConnect();
+    
+    $query = "SELECT ID, user_id, title, content, date_created FROM `posts` ";
+   // $result = $db->query($query) or die('Error inside utility.php');
+    
+    echo '<table border="0" cellspacing="2" cellpadding="2">
       <tr>
           <td> <font face="Arial">Title of post</font> </td>
           <td> <font face="Arial">User ID</font> </td>
-          <td> <font face="Arial">Category Name</font> </td>
           <td> <font face="Arial">Post ID</font> </td>
           <td> <font face="Arial">Body of Post</font> </td>
           <td> <font face="Arial">Date post was created</font> </td>
       </tr>';
+    
+    if ($result = $db->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $field1name = $row["title"];
+            $field2name = $row["user_id"];
+            $field3name = $row["ID"];
+            $field4name = $row["content"];
+            $field5name = $row["date_created"];
             
             
-            while ($row = $result->fetch_assoc()) {
-                $field1name = $row["title"];
-                $field2name = $row["user_id"];
-                $field3name = $row["ID"];
-                $field4name = $row["content"];
-                $field5name = $row["date_created"];
-                
-                
-                echo '<tr>
+            echo '<tr>
                   <td>'.$field1name.'</td>
                   <td>'.$field2name.'</td>
-                  <td>'.$catInfo['name'].'</td>
                   <td>'.$field3name.'</td>
                   <td>'.$field4name.'</td>
                   <td>'.$field5name.'</td>
               </tr>';
-            }
-            $result->free();
-            $db->close();
         }
-    }else{echo "Problem making query";}
+        $result->free();
+        $db->close();
+    } 
 }
 
 ?>
