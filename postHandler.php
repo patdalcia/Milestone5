e09b35b6f8c6dc24f68117cf06b5c31345036b7d
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-24 23:33:38 -0700

    aa

--- postHandler.php
+++ postHandler.php
@@ -4,7 +4,7 @@
     
     $title=trim($_POST['title']);
     $body=trim($_POST['body']);
-    $category=trim($_POST['category']);
+    $category=trim($_POST['Cat']);
     $results = 0;
     $catID = 0;
     if (!$title || !$body || !$category)
@@ -15,17 +15,7 @@
     $db = dbConnect();
     session_start();
     echo 'Made connections and session';
-    
-    $query = "SELECT catID FROM categories WHERE name = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param('s', $category);
-  
-    if($stmt->execute()){
-        $stmt->store_result();
-        if($stmt->num_rows > 0){
-            $stmt->bind_result($results);
-            if($stmt->fetch()){
-                $stmt->free_result();
+
                 
                 $query = "INSERT INTO posts (title, content, user_id, catID, date_created) VALUES (?, ?, ?, ?, NOW())";
                 $stmt = $db->prepare($query);
@@ -34,29 +24,6 @@
                 {
                     echo 'Post has been saved succsesfully!';
                 } else { echo 'Error: Could not save post';}
-            }
-        } else{  //Category could not be found
             
-            $query = "INSERT INTO categories (name, date) VALUES (?, NOW())";
-            $stmt = $db->prepare($query);
-            $stmt->bind_param('s', $category);
-            if($stmt->execute())
-            {
-                $catID = $db->insert_id;
-                $stmt->free_result();
-                
-                $query = "INSERT INTO posts (title, content, user_id, catID, date_created) VALUES (?, ?, ?, ?, NOW())";
-                $stmt = $db->prepare($query);
-                $stmt->bind_param('ssis', $title, $body, $_SESSION['ID'], $catID);
-                if($stmt->execute())
-                {
-                    echo 'Post has been saved succsesfully! Created new category!!';
-                } else { echo 'Error: Could not save post';}
-                
-                
-            } else { echo 'Error: Could not create new category';}
-        }
-    }
-    
-   
+        
 ?>
