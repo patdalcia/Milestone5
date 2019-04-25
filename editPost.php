35f10bc22187e1b3b280e0bc789e6b82156e64d3
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 14:34:39 -0700

    aa

--- editPost.php
+++ editPost.php
@@ -49,32 +49,31 @@ function createSelect(){
 
 if(isset($_POST["submitEditPost"])){
  
-    header("location: blog.php");
+    //header("location: blog.php");
     
-    
-    echo '
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="ISO-8859-1">
-    <link rel="stylesheet" href="styles/styles.css">
-    <title>My Blog Page</title>
-    
-    </head>
-    <body>
-    <h2>My Blog Page</h2>
-    
-    <form action="" method="post" id="usrform">
-    <table style="border: 0px;">
-    
-    <tr>
-    <td>Title of Post: <input type="text" name="title" value="<?php if(isset($title)){echo $title;}?>" maxlength="30"></td>
-    </tr>
-    <tr>
-    <td>Body of post: <textarea rows="4" cols="50" name="body" form="usrform">Enter text here...</textarea></td>
-    </tr>
-    <tr>
-    <td><?php createSelect();?></td>
+  echo '
+        <!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" href="styles/styles.css">
+<title>My Blog Page</title>
+
+</head>
+<body>
+<h2>My Blog Page</h2>
+
+<form action="postHandler.php" method="post" id="usrform">
+<table style="border: 0px;"> 
+
+ <tr> 
+ <td>Title of Post: <input type="text" name="title" maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>Body of post: <textarea rows="4" cols="50" name="body" form="usrform">Enter text here...</textarea></td>
+ </tr>
+  <tr> 
+ 	<td><?php selectBox()?></td>
   </tr>
   <tr>
   <td><input type="submit" value="Create Post"></td>
@@ -96,7 +95,6 @@ if(isset($_POST["submitEditPost"])){
 
 </body>
 </html>
-    
 ';
          
 }
