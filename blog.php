3ff2ffc26332925e0beaad8b579c2da6e9bbfec8
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-24 22:01:20 -0700

    ss

--- blog.php
+++ blog.php
@@ -104,48 +104,9 @@ if(isset($_POST['create'])) {
                 </select>
                 </p>';
           
-            if($_POST["catSelect"] != 0){
-                echo 'Inside isset';
-                $cat = $_POST["catSelect"];
-                $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
-                $stmt = $db->prepare($query);
-                $stmt->bind_param("i", $cat);
-                if($stmt->execute()){
-                    $stmt->bind_result($result);
-                    if($stmt->fetch()){
-                        echo '<table border="0" cellspacing="2" cellpadding="2">
-      <tr>
-          <td> <font face="Arial">Title of post</font> </td>
-          <td> <font face="Arial">User ID</font> </td>
-          <td> <font face="Arial">Category Name</font> </td>
-          <td> <font face="Arial">Post ID</font> </td>
-          <td> <font face="Arial">Body of Post</font> </td>
-          <td> <font face="Arial">Date post was created</font> </td>
-      </tr>';
-                        
-                        
-                        while ($row = $result->fetch_assoc()) {
-                            $field1name = $row["title"];
-                            $field2name = $row["user_id"];
-                            $field3name = $row["ID"];
-                            $field4name = $row["content"];
-                            $field5name = $row["date_created"];
-                            
-                            
-                            echo '<tr>
-                  <td>'.$field1name.'</td>
-                  <td>'.$field2name.'</td>
-                  <td>'.$catInfo['name'].'</td>
-                  <td>'.$field3name.'</td>
-                  <td>'.$field4name.'</td>
-                  <td>'.$field5name.'</td>
-              </tr>';
-                        }
-                        $result->free();
-                        $db->close();
-                    }
-                }
-            }// else if(!isset($_POST["catSelect"])){echo 'Please select a category!';}
+            if(isset($_POST["catSelect"])){
+                echo $_POST["catSelect"];
+            }
     
     
     
@@ -177,8 +138,49 @@ if(isset($_POST['create'])) {
 </table>';
 }
 
+/*
+if(isset($_POST["catSelect"]) && $_POST["catSelect"] != 0){
+    echo 'Inside isset';
+    $cat = $_POST["catSelect"];
+    $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("i", $cat);
+    if($stmt->execute()){
+        $stmt->bind_result($result);
+        if($stmt->fetch()){
+            echo '<table border="0" cellspacing="2" cellpadding="2">
+      <tr>
+          <td> <font face="Arial">Title of post</font> </td>
+          <td> <font face="Arial">User ID</font> </td>
+          <td> <font face="Arial">Category Name</font> </td>
+          <td> <font face="Arial">Post ID</font> </td>
+          <td> <font face="Arial">Body of Post</font> </td>
+          <td> <font face="Arial">Date post was created</font> </td>
+      </tr>';
+            
+            
+            while ($row = $result->fetch_assoc()) {
+                $field1name = $row["title"];
+                $field2name = $row["user_id"];
+                $field3name = $row["ID"];
+                $field4name = $row["content"];
+                $field5name = $row["date_created"];
+                
+                
+                echo '<tr>
+                  <td>'.$field1name.'</td>
+                  <td>'.$field2name.'</td>
+                  <td>'.$catInfo['name'].'</td>
+                  <td>'.$field3name.'</td>
+                  <td>'.$field4name.'</td>
+                  <td>'.$field5name.'</td>
+              </tr>';
+            }
+            $result->free();
+            $db->close();
+        }
+    }
+}// else if(!isset($_POST["catSelect"])){echo 'Please select a category!';}
 
-
-
-
+*/
 ?>
