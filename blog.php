5b12c02250c9ddc1b3fd1f73192bea2f0da37292
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-24 22:50:05 -0700

    ff

--- blog.php
+++ blog.php
@@ -91,7 +91,7 @@ if(isset($_POST['create'])) {
     
             echo '
             <form action="" method="post" id="catSelect">
-            <select name="catSelect"> 
+            <select name="cat"> 
             <option value="0">Select a category</option>';
             
             
@@ -99,6 +99,7 @@ if(isset($_POST['create'])) {
                 
                 $field = $row['catID'];
                 $field1 = $row['name'];
+                //echo '<option value="' . $field . '"name="' . $field1 . '>' . $field1 . '</option>';
                 echo '<option value="'. $field .'">'. $field1  .'</option>';
   
             }
@@ -186,47 +187,8 @@ if(isset($_POST["catSelect"]) && $_POST["catSelect"] != 0){
 
 */
 if(isset($_POST["ChooseCat"])){
-    echo 'Inside isset';
-    $cat = $_POST["catSelect"];
-    $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param("i", $cat);
-    if($stmt->execute()){
-        $stmt->bind_result($result);
-        if($stmt->fetch()){
-            echo '<table border="0" cellspacing="2" cellpadding="2">
-      <tr>
-          <td> <font face="Arial">Title of post</font> </td>
-          <td> <font face="Arial">User ID</font> </td>
-          <td> <font face="Arial">Category Name</font> </td>
-          <td> <font face="Arial">Post ID</font> </td>
-          <td> <font face="Arial">Body of Post</font> </td>
-          <td> <font face="Arial">Date post was created</font> </td>
-      </tr>';
-            
-            
-            while ($row = $result->fetch_assoc()) {
-                $field1name = $row["title"];
-                $field2name = $row["user_id"];
-                $field3name = $row["ID"];
-                $field4name = $row["content"];
-                $field5name = $row["date_created"];
-                
-                
-                echo '<tr>
-                  <td>'.$field1name.'</td>
-                  <td>'.$field2name.'</td>
-                  <td>'.$catInfo['name'].'</td>
-                  <td>'.$field3name.'</td>
-                  <td>'.$field4name.'</td>
-                  <td>'.$field5name.'</td>
-              </tr>';
-            }
-            $result->free();
-            $db->close();
-        }
-    }
-    
+    $cat = $_POST["cat"];
+    echo $cat;
 }
 
 ?>
