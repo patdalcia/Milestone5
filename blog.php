b7c6c97f40232f3c1dded319388488464a9e8aa9
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 00:07:46 -0700

    aaa

--- blog.php
+++ blog.php
@@ -134,56 +134,9 @@ if(isset($_POST['create'])) {
     $result->free();
     $db->close();
 } else if (isset($_POST['editPosts'])){
-    echo '<table style="border: 0px;">
-	<td>Edit posts was clicked</td>
-</table>';
+    header("location: editPost.php");
 }
 
-/*
-if(isset($_POST["catSelect"]) && $_POST["catSelect"] != 0){
-    echo 'Inside isset';
-    $cat = $_POST["catSelect"];
-    $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param("i", $cat);
-    if($stmt->execute()){
-        $stmt->bind_result($result);
-        if($stmt->fetch()){
-            echo '<table border="0" cellspacing="2" cellpadding="2">
-      <tr>
-          <td> <font face="Arial">Title of post</font> </td>
-          <td> <font face="Arial">User ID</font> </td>
-          <td> <font face="Arial">Category Name</font> </td>
-          <td> <font face="Arial">Post ID</font> </td>
-          <td> <font face="Arial">Body of Post</font> </td>
-          <td> <font face="Arial">Date post was created</font> </td>
-      </tr>';
-            
-            
-            while ($row = $result->fetch_assoc()) {
-                $field1name = $row["title"];
-                $field2name = $row["user_id"];
-                $field3name = $row["ID"];
-                $field4name = $row["content"];
-                $field5name = $row["date_created"];
-                
-                
-                echo '<tr>
-                  <td>'.$field1name.'</td>
-                  <td>'.$field2name.'</td>
-                  <td>'.$catInfo['name'].'</td>
-                  <td>'.$field3name.'</td>
-                  <td>'.$field4name.'</td>
-                  <td>'.$field5name.'</td>
-              </tr>';
-            }
-            $result->free();
-            $db->close();
-        }
-    }
-}// else if(!isset($_POST["catSelect"])){echo 'Please select a category!';}
-
-*/
 if(isset($_POST["ChooseCat"])){
     require 'functions/myfuncs.php';
     $db = dbConnect();
