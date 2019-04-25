05953ca45a25bf1a73855396de78c129f0612ed5
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-24 20:13:39 -0700

    fff

--- blog.php
+++ blog.php
@@ -77,13 +77,82 @@ if(isset($_POST['create'])) {
     } 
     
 } else if (isset($_POST['VpostsByCategories'])){
-    echo '<table style="border: 0px;">
-	<td>VpostsByCategories was clicked</td>
-</table>';
+    require 'functions/myfuncs.php';
+    $db = dbConnect();
+    $catInfo = "";
+    
+    $query = "SELECT name, catID FROM `categories` ";
+    $stmt = $db->prepare($query);
+    if($stmt->execute()){
+        $stmt->bind_result($catInfo);
+        if($stmt->fetch()){
+            $stmt->free_result();
+            
+            $query = "SELECT ID, user_id, title, content, date_created FROM `posts` WHERE catID = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("i", $catInfo['catID']);
+            if($stmt->execute()){
+                $stmt->bind_result($result);
+                if($stmt->fetch()){
+                    echo '<table border="0" cellspacing="2" cellpadding="2">
+      <tr>
+          <td> <font face="Arial">Title of post</font> </td>
+          <td> <font face="Arial">User ID</font> </td>
+          <td> <font face="Arial">Category Name</font> </td>
+          <td> <font face="Arial">Post ID</font> </td>
+          <td> <font face="Arial">Body of Post</font> </td>
+          <td> <font face="Arial">Date post was created</font> </td>
+      </tr>';
+                    
+                   
+                        while ($row = $result->fetch_assoc()) {
+                            $field1name = $row["title"];
+                            $field2name = $row["user_id"];
+                            $field3name = $row["ID"];
+                            $field4name = $row["content"];
+                            $field5name = $row["date_created"];
+                            
+                            
+                            echo '<tr>
+                  <td>'.$field1name.'</td>
+                  <td>'.$field2name.'</td>
+                  <td>'.$catInfo['name'].'</td>  
+                  <td>'.$field3name.'</td>
+                  <td>'.$field4name.'</td>
+                  <td>'.$field5name.'</td>
+              </tr>';
+                        }
+                        $result->free();
+                        $db->close();
+                    
+                }
+            }
+        }
+    }
+    
+    
 } else if (isset($_POST['Vcategories'])){
-    echo '<table style="border: 0px;">
-	<td>Vcategories was clicked</td>
-</table>';
+    require 'functions/myfuncs.php';
+    
+    $db = dbConnect();
+    
+    echo '<table border="0" cellspacing="2" cellpadding="2">
+      <tr>
+          <td> <font face="Arial">Category Name</font> </td>
+      </tr>';
+    
+    $query = 'SELECT name FROM categories';
+    if ($result = $db->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $field1name = $row["name"];
+            echo '<tr>
+                  <td>'.$field1name.'</td>
+              </tr>';
+        }
+    }
+    echo '</table>';
+    $result->free();
+    $db->close();
 } else if (isset($_POST['editPosts'])){
     echo '<table style="border: 0px;">
 	<td>Edit posts was clicked</td>
