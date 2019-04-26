197c2acbf2897fb5c5cf581450cd9fc47b5cd858
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-26 00:31:56 -0700

    added admin functionality

--- loginHandler.php
+++ loginHandler.php
@@ -24,6 +24,7 @@ for each userName and resets this number after a succesful login -->
 	$username=trim($_POST['username']);
 	$password=trim($_POST['password']);
 	$id = 0;
+	$user_role = 1;
 	
 	//making sure that theres input for both username and password fields
 	if (!$password || !$username)
@@ -35,14 +36,14 @@ for each userName and resets this number after a succesful login -->
 	//establishing connection to database
 	$db = dbConnect();
 	
-	$query = "SELECT ID FROM users WHERE userName = ?";
+	$query = "SELECT ID, user_role FROM users WHERE userName = ?";
 	$stmt = $db->prepare($query);
 	$stmt->bind_param('s', $username);
 	if($stmt->execute() or die("Encountered a problem connecting to database(query INSERT 1)"))
 	{
 	    $stmt->store_result();
 	    if($stmt->num_rows > 0){ 
-	        $stmt->bind_result($id);
+	        $stmt->bind_result($id, $user_role);
 	        if($stmt->fetch()){
 	            $stmt->free_result();
 	            $query = "SELECT id, password FROM users_passwords WHERE id = ? and password = ?";
@@ -56,6 +57,7 @@ for each userName and resets this number after a succesful login -->
 	                if($stmt->fetch()){
 	                    $_SESSION['ID'] = $id;
 	                    $_SESSION['password'] = $password;
+	                    $_SESSION['user_role'] = $user_role;
 	                    $stmt->free_result();
 	                    $db->close();
 	                    if (isset($_COOKIE['login'])) {
