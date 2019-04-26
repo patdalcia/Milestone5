9c5280c38c9c793a12acf577e63d8209bfaf3cb1
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 19:57:25 -0700

    gg

--- editPost.php
+++ editPost.php
@@ -2,8 +2,8 @@
 <?php 
 require 'functions/myfuncs.php';
 $db = dbConnect();
-
-$query = "SELECT title FROM 'posts' WHERE ID =" . $_POST["selectedPost"];
+$postID = $_POST["selectedPost"];
+$query = "SELECT title FROM 'posts' WHERE ID = $postID";
 $results = $db->query($query);
 
 foreach($results as $row){
