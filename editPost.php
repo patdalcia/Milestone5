dc0c508c53ac46c01088de3eef8bd489edd748ae
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 19:51:47 -0700

    g

--- editPost.php
+++ editPost.php
@@ -1,18 +1,4 @@
 
 <?php 
-function getTitle(){
-require 'functions/myfuncs.php';
-$db = dbConnect();
-
-
-$query = "SELECT title FROM 'posts' WHERE ID = " . $_POST["selectedPost"];;
-$postInfo = $db->query($query);
-$info = $postInfo["title"];
-$db->close();
-return $info;
-}
-
-
-  $yeet = getTitle();
-  echo $yeet;
+echo $_POST["selectedPost"];
 ?>
