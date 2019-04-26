132bb1fa3457e00af3940adf41be7b7d584103cf
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-26 02:17:41 -0700

    ff

--- _editUsers.php
+++ _editUsers.php
@@ -2,13 +2,6 @@
 require 'functions/myfuncs.php';
 session_start();
 
-$_SESSION['userID'] = $_POST['userID'];
-$_SESSION['firstName'] = $_POST['firstName'];
-$_SESSION['lastName'] = $_POST['lastName'];
-$_SESSION['emailAddress'] = $_POST['emailAddress'];
-$_SESSION['userName'] = $_POST['userName'];
-$_SESSION['user_role'] = $_POST['user_role'];
-
 $userID = $_SESSION['userID'];
 $firstName = $_SESSION['firstName'];
 $lastName = $_SESSION['lastName'];
