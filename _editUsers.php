29e0e1708ad5e2d367e0ea23064ca1a4f085bc07
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-26 02:15:12 -0700

    ff

--- _editUsers.php
+++ _editUsers.php
@@ -2,7 +2,19 @@
 require 'functions/myfuncs.php';
 session_start();
 
-$name = $_SESSION['firstName'];
+$_SESSION['userID'] = $_POST['userID'];
+$_SESSION['firstName'] = $_POST['firstName'];
+$_SESSION['lastName'] = $_POST['lastName'];
+$_SESSION['emailAddress'] = $_POST['emailAddress'];
+$_SESSION['userName'] = $_POST['userName'];
+$_SESSION['user_role'] = $_POST['user_role'];
 
-echo $name;
+$userID = $_SESSION['userID'];
+$firstName = $_SESSION['firstName'];
+$lastName = $_SESSION['lastName'];
+$emailAddress = $_SESSION['emailAddress'];
+$userName = $_SESSION['userName'];
+$user_role = $_SESSION['user_role'];
+
+echo $userID . $firstName . $lastName . $emailAddress . $userName . $user_role;
 ?>
