fc6eeb95bb6dafbe595fe68425426464a1d252a6
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 20:00:30 -0700

    fff

--- editPost.php
+++ editPost.php
@@ -2,11 +2,22 @@
 <?php 
 require 'functions/myfuncs.php';
 $db = dbConnect();
-$postID = $_POST["selectedPost"];
-$query = "SELECT title FROM 'posts' WHERE ID = $postID";
-$results = $db->query($query);
 
-foreach($results as $row){
-    echo $row["title"];
+echo '<table border="0" cellspacing="2" cellpadding="2">
+      <tr>
+          <td> <font face="Arial">Post name</font> </td>
+      </tr>';
+
+$query = "SELECT title FROM posts";
+if ($result = $db->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["title"];
+        echo '<tr>
+                  <td>'.$field1name.'</td>
+              </tr>';
+    }
 }
+echo '</table>';
+$result->free();
+$db->close();
 ?>
