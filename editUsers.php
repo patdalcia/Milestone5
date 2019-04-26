cae5d1f881dae5577bd2b0e298cdbf911028a4c3
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-26 01:36:25 -0700

    ff

--- editUsers.php
+++ editUsers.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'functions/myfuncs.php';
 checkSessionTime();
+function getUserList(){
+require_once 'functions/myfuncs.php';
 $db = dbConnect();
 $row = "";
 
@@ -15,6 +16,51 @@ foreach($results as $row){
     $userName = $row['userName'];
     $user_role = $row['user_role'];
     
-    echo nl2br($ID . $firstName . $lastName . $emailAddress . $userName . $user_role . "\n");
+    echo '
+    <form action="" method="post" id="editform">
+<table style="border: 0px;"> 
+
+ <tr> 
+ <td>User ID: <input type="text" name="userID" value="'. $ID .'"  maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>User ID: <input type="text" name="firstName" value="' . $firstName . '"  maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>User ID: <input type="text" name="lastName" value="' . $lastName . '"  maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>Email Address: <input type="text" name="emailAddress" value="' . $emailAddress . '"  maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>UserName: <input type="text" name="userName" value="' . $userName . '"  maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>User role: <input type="text" name="user_role" value="' . $user_role . '"  maxlength="30"></td>
+ </tr>
+  <tr>
+  <td><input type="submit" name="editUser" value="Update user"></td>
+  <td><input type="submit" name="deletePost" value="Delete user"></td>
+  </tr>
+ </table>
+</form> ';
+}
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" href="styles/styles.css">
+<title>Edit post</title>
+
+</head>
+<body>
+<h2>Edit user page</h2>
+<?php 
+getUserList();
+?>
+
+</body>
+</html>
