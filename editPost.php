dcddce876111c7a1ba8b37d1cfe1a0e2aa55a6d9
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 19:50:30 -0700

    gg

--- editPost.php
+++ editPost.php
@@ -9,21 +9,10 @@ $query = "SELECT title FROM 'posts' WHERE ID = " . $_POST["selectedPost"];;
 $postInfo = $db->query($query);
 $info = $postInfo["title"];
 $db->close();
-echo $info;
+return $info;
 }
 
-function getContent(){
-    require 'functions/myfuncs.php';
-    $db = dbConnect();
-    
-    
-    $query = "SELECT content FROM 'posts' WHERE ID = " . $_POST["selectedPost"];;
-    $postInfo = $db->query($query);
-    
-    
-    $db->close();
-   echo $postInfo["content"];
-}
 
-getTitle();
+  $yeet = getTitle();
+  echo $yeet;
 ?>
