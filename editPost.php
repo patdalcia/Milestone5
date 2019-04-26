b18b9e3736dffa558111b232f8c465393d49f2b3
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 19:44:55 -0700

    gg

--- editPost.php
+++ editPost.php
@@ -24,34 +24,7 @@ function getContent(){
     $db->close();
     return $postInfo["content"];
 }
+
+echo getTitle();
+echo getContent();
 ?>
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<link rel="stylesheet" href="styles/styles.css">
-<title>Edit blog post</title>
-
-</head>
-<body>
-<h2>Edit your post</h2>
-
-<form action="" method="post" id="editfrm">
-<table style="border: 0px;"> 
-
- <tr> 
- <td>Title of Post: <input type="text" name="title" value="<?php echo getTitle();?>"  maxlength="30"></td>
- </tr>
- <tr>
- <td>Body of post: <textarea rows="4" cols="50" name="body" form="usrform"><?php echo getContent();?></textarea></td>
- </tr>
-  <tr>
-  <td><input type="submit" value="Create Post"></td>
-  </tr>
- </table>
-</form>
-</body>
-</html>
