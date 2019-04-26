5b3165b8f5f2c0bdb218c4fd7f4ef05061aa1a27
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 20:19:16 -0700

    fff

--- editPost.php
+++ editPost.php
@@ -1,6 +1,6 @@
 
 <?php 
-function getTitle(){
+function getInfo(){
 require 'functions/myfuncs.php';
 $db = dbConnect();
 $postID = $_POST["selectedPost"];
@@ -15,25 +15,32 @@ if ($result = $db->query($query)) {
 $result->free();
 $db->close();
 }
-
-function getContent(){
-    require 'functions/myfuncs.php';
-    $db = dbConnect();
-    $postID = $_POST["selectedPost"];
-    
-    $query = "SELECT content FROM posts WHERE ID = $postID";
-    if ($result = $db->query($query)) {
-        $row = $result->fetch_assoc();
-        $field1name = $row["content"];
-        $db->close();
-        return $field1name;
-    }
-    $result->free();
-    $db->close();
-}
-
-
-$info = getTitle();
-echo $info["title"] . $info["content"];
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" href="styles/styles.css">
+<title>Edit post</title>
+
+</head>
+<body>
+<h2>Edit post page</h2>
+
+<form action="" method="post" id="editform">
+<table style="border: 0px;"> 
+
+ <tr> 
+ <td>Title of Post: <input type="text" name="title" maxlength="30"></td>
+ </tr>
+ <tr>
+ <td>Body of post: <textarea rows="4" cols="50" name="body" form="usrform">Enter text here...</textarea></td>
+ </tr>
+  <tr>
+  <td><input type="submit" value="Create Post"></td>
+  </tr>
+ </table>
+</form>
+</body>
+</html>
