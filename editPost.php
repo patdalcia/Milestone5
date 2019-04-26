da577573ee41dc140f7f874d23e8814209ee2911
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 20:10:40 -0700

    gg

--- editPost.php
+++ editPost.php
@@ -4,10 +4,6 @@ function getTitle(){
 require 'functions/myfuncs.php';
 $db = dbConnect();
 $postID = $_POST["selectedPost"];
-echo '<table border="0" cellspacing="2" cellpadding="2">
-      <tr>
-          <td> <font face="Arial">Post name</font> </td>
-      </tr>';
 
 $query = "SELECT title FROM posts WHERE ID = $postID";
 if ($result = $db->query($query)) {
@@ -20,6 +16,22 @@ $result->free();
 $db->close();
 }
 
+function getContent(){
+    require 'functions/myfuncs.php';
+    $db = dbConnect();
+    $postID = $_POST["selectedPost"];
+    
+    $query = "SELECT content FROM posts WHERE ID = $postID";
+    if ($result = $db->query($query)) {
+        $row = $result->fetch_assoc();
+        $field1name = $row["content"];
+        $db->close();
+        return $field1name;
+    }
+    $result->free();
+    $db->close();
+}
 
-echo getTitle();
+echo getTitle() . getContent();
+
 ?>
