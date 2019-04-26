5dc3980dac54f9ed01ff12a5529453231a993f34
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-26 02:34:52 -0700

    ff

--- _editUsers.php
+++ _editUsers.php
@@ -1,5 +1,6 @@
 <?php 
 require 'functions/myfuncs.php';
+checkSessionTime();
 session_start();
 
 $userID = $_SESSION['userID'];
@@ -8,6 +9,19 @@ $lastName = $_SESSION['lastName'];
 $emailAddress = $_SESSION['emailAddress'];
 $userName = $_SESSION['userName'];
 $user_role = $_SESSION['user_role'];
-
+    
+    $db = dbConnect();
+    session_start();
+    
+    $query = "UPDATE users SET firstName= ?, lastName = ?, emailAddress = ?, userName = ?, user_role = ? WHERE ID = ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('ssssii', $firstName, $lastName, $emailAddress, $userName, $user_role, $userID);
+    if($stmt->execute())
+    {
+        $db->close();
+        echo '<script language="javascript">alert("Post was updated succsesfully! Click ok to return to main blog page.")</script>';
+        echo '<script language="javascript">location.replace("blog.php");</script>';
+    } else { echo 'Error: Could not save post';}
+    
 
 ?>
