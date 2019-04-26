974241a9da3f2e6f6d1eddded969bd004f776ce5
patdalcia <49659510+patdalcia@users.noreply.github.com>
2019-04-25 21:44:34 -0700

    gg

--- _editPost.php
+++ _editPost.php
@@ -1,9 +1,27 @@
 <?php
-//require 'functions/myfuncs.php';
+require 'functions/myfuncs.php';
 
 $title=trim($_POST['editTitle']);
 $content=trim($_POST['editContent']);
 $postID = $_POST['postID'];
 
-echo $postID;
+
+if (!$title || !$content)
+{
+    echo '<script language="javascript">alert("Input fields were left blank! Click OK to try again.")</script>';
+    echo '<script language="javascript">location.replace("editPost.php");</script>';
+}
+$db = dbConnect();
+session_start();
+echo 'Made connections and session';
+
+
+$query = "UPDATE posts SET title = ?, content= ? WHERE ID = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param('ssi', $title, $content, $postID);
+if($stmt->execute())
+{
+    $db->close();
+    header("location: blog.php");
+} else { echo 'Error: Could not save post';}
 ?>
